37ed7874da4d5817978adadca2fca6f793282306
Viklysx <vikbelec111@yandex.ru>
2021-05-17 01:48:13 +0300

    feat: add ContainerBanner component

--- decomposition/src/components/ContainerBanner/ContainerBanner.js
+++ decomposition/src/components/ContainerBanner/ContainerBanner.js
@@ -1,7 +1,7 @@
 export default function ContainerBanner(props) {
     return (
-        <div>
-            
+        <div className="banner">
+            <img src="https://ds03.infourok.ru/uploads/ex/0bf3/00022ed2-9b71b257/hello_html_m77ede67d.png" alt="banner" />
         </div>
     )
 }
